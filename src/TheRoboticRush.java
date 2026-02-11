cc4886a2dd08a6a4e79168a16b4aab2e3a77dac4
Abhay <abhayanilark@gmail.com>
2026-02-11 22:53:30 +0530

    TheRoboticRush corrected

--- src/TheRoboticRush.java
+++ src/TheRoboticRush.java
@@ -5,20 +5,20 @@ import java.util.*;
 public class TheRoboticRush {
 
 
-    public static int upper_bound(int num,List<Integer> a){
-        if(a.isEmpty()) return -1;
-        int lo = 0, hi = a.size()-1;
-        while(hi-lo>1){
-            int mid = (hi+lo)/2;
-            if(a.get(mid)<=num){lo=mid;}
-            else{hi=mid;}
-        }
-        if(a.get(lo)>num) return lo;
-        if(a.get(hi)>num) return hi;
-
-        if(a.get(hi)<=num) return a.size();
-        return -1;
-    }
+//    public static int upper_bound(int num,List<Integer> a){
+//        if(a.isEmpty()) return -1;
+//        int lo = 0, hi = a.size()-1;
+//        while(hi-lo>1){
+//            int mid = (hi+lo)/2;
+//            if(a.get(mid)<=num){lo=mid;}
+//            else{hi=mid;}
+//        }
+//        if(a.get(lo)>num) return lo;
+//        if(a.get(hi)>num) return hi;
+//
+//        if(a.get(hi)<=num) return a.size();
+//        return -1;
+//    }
 
     public static int lower_bound(int num,List<Integer> a){
         if(a.isEmpty()) return -1;
@@ -28,11 +28,35 @@ public class TheRoboticRush {
             if(a.get(mid)<=num){lo=mid;}
             else{hi=mid;}
         }
-
+        if(a.get(hi)==num) return hi;
         if(a.get(lo)>=num) return lo;
         if(a.get(hi)>=num) return hi;
-
         if(a.get(hi)<=num) return a.size();
+        return -1;
+    }
+
+    public static boolean check(int i,int j,int a,int b){
+        if(i!=-1 && a>=i) return true;
+        if(j!=-1 && b>=j) return true;
+        return false;
+    }
+
+    public static int findInd(int leftB,int rightB,List<Integer>left,List<Integer>right,int k){
+        int lo=0,hi=k-1;
+        while(hi-lo>1){
+            int mid = (hi+lo)/2;
+            if(check(leftB,rightB,left.get(mid),right.get(mid))){
+                hi=mid;
+            } else{
+                lo=mid;
+            }
+        }
+        if(check(leftB,rightB,left.get(lo),right.get(lo))){
+            return lo;
+        }
+        if(check(leftB,rightB,left.get(hi),right.get(hi))){
+            return hi;
+        }
         return -1;
     }
 
@@ -60,51 +84,59 @@ public class TheRoboticRush {
         List<Integer> left = new ArrayList<>();
         List<Integer> right = new ArrayList<>();
 
-        int death = 0;
+        int left_max=0,right_max=0,pos=0;
+        for(int i=0;i<k;i++){
+            if(s.charAt(i)=='L'){
+                pos--;
+                if(pos<0 && Math.abs(pos)>left_max){
+                    left_max = Math.abs(pos);
+                }
+            } else{
+                pos++;
+                if(pos>right_max){
+                    right_max = pos;
+                }
+            }
+            left.add(left_max);
+            right.add(right_max);
+        }
+
+        List<Integer> ans = new ArrayList<>();
+        for(int i=0;i<k;i++){ans.add(0);}
+
         for(int i=0;i<n;i++){
             int num = a.get(i);
             int ind = lower_bound(num,b);
-            if(ind!=-1 && ind<b.size() && b.get(ind)==num){
-                death++;
-            } else {
-                if (ind != -1 && ind<b.size()) {
-                    right.add(b.get(ind) - num);
-                }
-                ind--;
-                if (ind != -1 ) {
-                    left.add(num - b.get(ind));
-                }
+            int leftB=-1,rightB=-1;
+
+            if(ind==-1){
+            }else if(ind==b.size()){
+                leftB = (num - b.get(ind-1));
+            }else {
+                rightB = (b.get(ind)-num);
+                if(ind-1>=0){
+                    leftB = (num - b.get(ind-1));}
             }
+
+            ind = findInd(leftB,rightB,left,right,k);
+
+            if(ind!=-1){
+                ans.set(ind,ans.get(ind)+1);
+            }
+
         }
 
-        Collections.sort(left);
-        Collections.sort(right);
 
-        int pos = 0;
+
+
+
+
+        int sum =0;
         for(int i=0;i<k;i++){
-            char c= s.charAt(i);
-            if(c=='L'){
-                pos--;
-                if(pos<0) {
-                    int ind = upper_bound(Math.abs(pos), left);
-                    if (ind > 0) {
-                        death += ind;
-                        left.subList(0, ind).clear();
-                    }
-                }
-            }else{
-                pos++;
-                if(pos>0) {
-                    int ind = upper_bound(Math.abs(pos), right);
-                    if (ind > 0) {
-                        death += ind;
-                        right.subList(0, ind).clear();
-                    }
-                }
-            }
-            System.out.print( (n -death) + " ");
-        }
-        System.out.println();
+            sum+=ans.get(i);
+            System.out.print( n - sum + " ");
+        }System.out.println();
+
     }
 
     public static void main(String args[]){
