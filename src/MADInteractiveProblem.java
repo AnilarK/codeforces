b29d09a60ce405eb4ab8d6acd8835d18ec77ac52
Abhay <abhayanilark@gmail.com>
2026-02-24 09:12:48 +0530

    MAD Interactive Problem Initial Commit

---
+++ src/MADInteractiveProblem.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+//      https://codeforces.com/problemset/problem/2159/A
+
+public class MADInteractiveProblem {
+
+    public static void solve(Scanner sc){
+
+
+    }
+
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t>0){
+            solve(sc); t--;}
+    }
+}
