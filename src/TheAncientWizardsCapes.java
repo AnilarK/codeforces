8d5798239b08f9797256a913d49792627deb00c6
Abhay <abhayanilark@gmail.com>
2026-02-25 17:36:21 +0530

    TheAncientWizardsCapes initial commit

---
+++ src/TheAncientWizardsCapes.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+//  https://codeforces.com/problemset/problem/2155/C
+
+public class TheAncientWizardsCapes {
+
+    public static void solve(Scanner sc){
+        int n = sc.nextInt();
+        int mo = 676767677;
+        List<Integer> a = new ArrayList<>();
+        for(int i=0;i<n;i++){
+            a.add(sc.nextInt());
+        }
+
+
+
+
+    }
+
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int t =  sc.nextInt();
+        while(t>0){
+            solve(sc);
+            t--;
+        }
+    }
+}
