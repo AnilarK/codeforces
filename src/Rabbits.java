079843d86dcc0c32ec1a4ca1c60fe0b471f348c8
Abhay <abhayanilark@gmail.com>
2026-02-28 13:56:07 +0530

    Rabbits initial commit

---
+++ src/Rabbits.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+//  https://codeforces.com/problemset/problem/2147/C
+
+public class Rabbits {
+
+    public void solve(Scanner sc){
+        int n = sc.nextInt();
+        sc.nextLine();
+        StringBuilder s = new StringBuilder(sc.nextLine());
+
+
+    }
+
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        Rabbits obj = new Rabbits();
+        while(t>0){
+            obj.solve(sc);
+            t--;
+        }
+    }
+}
