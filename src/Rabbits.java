17b11291f49a687aa4e240d25ddb5900343654ae
Abhay <abhayanilark@gmail.com>
2026-03-01 22:34:55 +0530

    Rabbits second commit

--- src/Rabbits.java
+++ src/Rabbits.java
@@ -9,7 +9,27 @@ public class Rabbits {
         sc.nextLine();
         StringBuilder s = new StringBuilder(sc.nextLine());
 
+        for(int i=0;i<n;i++){
+            int cnt = 0 , j=i;
+            while(j<n && s.charAt(j)=='0'){
+                cnt++; j++;
+            }
 
+            if(cnt>1){
+                i = j + 1 ;
+            } else if(cnt==1){
+                if(i==0 || i==n-1 || (i<n-2 && s.charAt(i+2)=='0') ){
+                    continue;
+                } else if( i>1 && s.charAt(i-2)=='0'){
+                    s.setCharAt(i,'1');
+                } else {
+                    System.out.println("NO");
+                    return;
+                }
+            }
+
+        }
+        System.out.println("YES");
     }
 
     public static void main(String args[]){
