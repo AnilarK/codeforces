3774d39fa8d5ff0d69443e01a517516da867218e
Abhay <abhayanilark@gmail.com>
2026-03-01 23:26:46 +0530

    MaxSumOREasyVersion initial commit

---
+++ src/MaxSumOREasyVersion.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+//  https://codeforces.com/problemset/problem/2146/D1
+
+public class MaxSumOREasyVersion {
+
+    public void solve(Scanner sc){
+        int l = sc.nextInt();
+        int r = sc.nextInt();
+
+        int n = r - l + 1;
+
+
+
+    }
+
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        MaxSumOREasyVersion obj = new MaxSumOREasyVersion();
+        while(t>0){
+            obj.solve(sc);
+            t--;
+        }
+    }
+}
