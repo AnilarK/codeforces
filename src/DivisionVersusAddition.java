d0b65cabfe700b8fa4663709a7a6878c947ba58a
Abhay <abhayanilark@gmail.com>
2026-03-04 19:17:27 +0530

    DivisionVersusAddition final commit

--- src/DivisionVersusAddition.java
+++ src/DivisionVersusAddition.java
@@ -4,32 +4,9 @@ import java.util.*;
 
 public class DivisionVersusAddition {
 
-    public int findOp(long num){
-        int cnt = 0;
-        while(num>1){
-            num = num/2;
-            if(num!=1){num++;}
-            cnt++;
-        }
-        return cnt;
-    }
-
-    public void resolve(long num, List<Long> a, List<Long> fre){
-        int x = findOp(num);
-        int x1 = findOp(num+1L);
-        a.add(a.getLast() + x1);
-
-        if(x1!=x){
-            fre.add(fre.getLast() + 1);
-        } else {
-            fre.add(fre.getLast());
-        }
-    }
-
     public void solve(Scanner sc){
         int n = sc.nextInt();
         int q = sc.nextInt();
-
         List<Long> a = new ArrayList<>();
         a.add(0L);
         for(int i=0;i<n;i++){
@@ -41,9 +18,31 @@ public class DivisionVersusAddition {
         pre.add(0L);
         fre.add(0L);
 
-        for(int i=1;i<=n;i++){
-            resolve(a.get(i),pre,fre);
+        Set<Long> spc = new HashSet<>();
+        long k =1;
+        for(int i=0;i<40;i++){
+            k = k*2L;
+            spc.add(k+1);
         }
+
+        long sum = 0;
+        for(int i=1;i<=n;i++){
+            long num = a.get(i), cnt=0;
+            long f = fre.getLast();
+            if(spc.contains(num)){f++;}
+            while(num>1){
+                num = num/2;
+                if(num!=1){num++;}
+                cnt++;
+            }
+
+            sum += cnt;
+            pre.add(sum);
+            fre.add(f);
+        }
+
+//        System.out.println(fre);
+//        System.out.println(pre);
 
         for(int i = 0;i<q;i++){
             int l = sc.nextInt();
@@ -51,11 +50,12 @@ public class DivisionVersusAddition {
 
             long ans = pre.get(r) - pre.get(l-1);
             long f = fre.get(r) - fre.get(l-1);
-
-            if(f!=0){
-                ans -= (f+1)/2; }
+            if(f>0){
+                ans += f/2; }
             System.out.println(ans);
+
         }
+
     }
 
     public static void main(String args[]){
