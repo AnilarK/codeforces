d889534bc49082b31e2336b91ee0fff56c040e76
Abhay <abhayanilark@gmail.com>
2026-03-06 22:17:34 +0530

    GameOnArray final commit

--- src/GameOnArray.java
+++ src/GameOnArray.java
@@ -6,13 +6,34 @@ public class GameOnArray {
 
     public void solve(Scanner sc){
         int n = sc.nextInt();
-        List<Integer> a = new ArrayList<>();
+        Map<Long,Long> m = new HashMap<>();
+        long sum = 0;
         for(int i=0;i<n;i++){
-            a.add(sc.nextInt());
+            long num = sc.nextLong();
+            sum += num;
+            m.put(num,m.getOrDefault(num,0L)+1);
         }
 
+        List<Long> a = new ArrayList<>();
+        for(Map.Entry<Long,Long> ent : m.entrySet() ){
+            long freq = ent.getKey();
+            if(freq%2==1){
+                a.add(ent.getValue());
+                sum -= a.getLast();
+            }
+        }
+        long ans1 = sum/2L , ans2 = sum/2L;
 
+        a.sort(null);
+        for(int i=a.size()-1;i>=0;i--){
+            ans1 += a.get(i);
+            i--;
+            if(i>=0){
+                ans2 += a.get(i);
+            }
+        }
 
+        System.out.println(ans1 + " " + ans2);
     }
 
     public static void main(String args[]){
