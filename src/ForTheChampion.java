e795b2d113cd6e05cb0b55e365db2d9d8a87f6be
Abhay <abhayanilark@gmail.com>
2026-03-06 22:31:42 +0530

    ForTheChampion

---
+++ src/ForTheChampion.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+//  https://codeforces.com/problemset/problem/2135/B
+
+public class ForTheChampion {
+
+    public void solve(Scanner sc){
+
+    }
+
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        ForTheChampion obj = new ForTheChampion();
+        while(t>0){
+            obj.solve(sc);
+            t--;
+        }
+    }
+}
