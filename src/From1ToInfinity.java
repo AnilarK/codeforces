84c363204aa83f7331cad913ddaa0abe0ccf8814
Abhay <abhayanilark@gmail.com>
2026-03-08 02:56:09 +0530

    From1ToInfinity initial commit

---
+++ src/From1ToInfinity.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+//  https://codeforces.com/problemset/problem/2132/D
+
+public class From1ToInfinity {
+
+    public void solve(Scanner sc){
+        long k = sc.nextLong();
+
+
+    }
+
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        From1ToInfinity obj = new From1ToInfinity();
+        while(t>0){
+            obj.solve(sc);
+            t--;
+        }
+    }
+}
